8ef561ad0e9e74d4d0af0e2632a7b8cf83cb3d09
Zekosz <dani.winberg@gmail.com>
2023-11-15 19:41:52 +0100

    Add login component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,16 @@
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <LogIn/>
 </template>
 
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import LogIn from './components/LogIn.vue';
 import { onMounted } from 'vue';
 
 export default {
+  components: {
+    LogIn
+  },
   setup() {
     onMounted(() => {
       fetch('/api')
---
+++ frontend/src/components/LogIn.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Logga in</h2>
+    <form @submit.prevent="login">
+      <label for="username">E-post:</label>
+      <input type="text" id="username" v-model="username" />
+
+      <label for="password">Lösenord:</label>
+      <input type="password" id="password" v-model="password" />
+
+      <button type="submit">Logga in</button>
+    </form>
+  </div>
+  <div>
+    <p>Eller skapa konto nedan</p>
+    <button type="submit">Skapa konto</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      console.log('Logging in with:', this.username, this.password);
+    }
+  }
+};
+</script>
