7b19a30408704790a2dee2b85fc369a328fd7a3b
Zekosz <dani.winberg@gmail.com>
2023-11-22 11:16:50 +0100

    Add user to database

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,47 @@
-const express = require('express'),
-  path = require('path')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const dotenv = require('dotenv');
+const { Client } = require('pg');
 
-const app = express()
-
-const dotenv = require('dotenv'),
-{ Client } = require('pg')
-
-dotenv.config()
+dotenv.config();
 
 const client = new Client({
-connectionString: process.env.PGURI
-})
+  connectionString: process.env.PGURI,
+});
 
-client.connect()
+client.connect();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api', async (_request, response) => {
-  const { rows } = await client.query(
-    'SELECT * FROM users',
+  try {
+    const { rows } = await client.query('SELECT * FROM users');
+    response.send(rows);
+  } catch (error) {
+    console.error('Error:', error);
+    response.status(500).json({ message: 'Server error' });
+  }
+});
 
-  )
+app.post('/api/register', async (req, res) => {
+  try {
+    const { email, password, firstName, lastName } = req.body;
 
-  response.send(rows)
-})
+    const query = 'INSERT INTO users (email, password, first_name, last_name) VALUES ($1, $2, $3, $4)';
+    const result = await client.query(query, [email, password, firstName, lastName]);
 
-app.use(express.static(path.join(path.resolve(), 'dist')))
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+app.use(express.static(path.join(path.resolve(), 'dist')));
 
 app.listen(3000, () => {
-  console.log('Redo på http://localhost:3000/')
-})
+  console.log('Server running at http://localhost:3000/');
+});
--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,45 +1,60 @@
 <template>
-    <form @submit.prevent="registerUser">
-      <div>
-        <label for="email">E-post:</label>
-        <input type="email" id="email" v-model="user.email">
-      </div>
-      <div>
-        <label for="password">Lösenord:</label>
-        <input type="password" id="password" v-model="user.password">
-      </div>
-      <div>
-        <label for="firstName">Förnamn:</label>
-        <input type="text" id="firstName" v-model="user.firstName">
-      </div>
-      <div>
-        <label for="lastName">Efternamn:</label>
-        <input type="text" id="lastName" v-model="user.lastName">
-      </div>
-      <button type="submit">Skapa konto</button>
-    </form>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        user: {
-          email: '',
-          password: '',
-          firstName: '',
-          lastName: ''
+  <form @submit.prevent="registerUser">
+    <div>
+      <label for="email">E-post:</label>
+      <input type="email" id="email" v-model="user.email">
+    </div>
+    <div>
+      <label for="password">Lösenord:</label>
+      <input type="password" id="password" v-model="user.password">
+    </div>
+    <div>
+      <label for="firstName">Förnamn:</label>
+      <input type="text" id="firstName" v-model="user.firstName">
+    </div>
+    <div>
+      <label for="lastName">Efternamn:</label>
+      <input type="text" id="lastName" v-model="user.lastName">
+    </div>
+    <button type="submit">Skapa konto</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+        firstName: '',
+        lastName: ''
+      }
+    };
+  },
+  methods: {
+    async registerUser() {
+      try {
+        const response = await fetch('http://localhost:3000/api/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(this.user),
+        });
+
+        if (response.ok) {
+          console.log('User registered successfully');
+        } else {
+          console.error('Error registering user');
         }
-      };
-    },
-    methods: {
-      registerUser() {
-        console.log(this.user);
+      } catch (error) {
+        console.error('Network error:', error);
       }
     }
-  };
-  </script>
-  
-  <style>
-  </style>
-  
+  },
+};
+</script>
+
+<style>
+</style>
