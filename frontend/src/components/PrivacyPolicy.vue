bec98e70b4718dced6b90e95757db280aa9d647c
cwacode <williamandersson27@gmail.com>
2023-11-26 23:56:24 +0100

    changed privacy policy added register

--- frontend/src/components/PrivacyPolicy.vue
+++ frontend/src/components/PrivacyPolicy.vue
@@ -1,27 +1,16 @@
 <template>
     <div class="privacy-policy">
       <h2>Integritetspolicy</h2>
-      <p>
-
-Välkommen till Loginsite AB. Vi är engagerade i att skydda din integritet och dina personuppgifter. Denna integritetspolicy förklarar hur vi samlar in, använder och skyddar de personuppgifter du delar med oss.
-
-<h4>Information som samlas in</h4>
-När du använder vår webbplats, samlar vi in personuppgifter som ditt namn, efternamn, e-postadress och lösenord. Dessa uppgifter används för att skapa och hantera ditt konto, samt för att förbättra din användarupplevelse.
-
-<h4>Användning av information</h4>
-Din information används för att tillhandahålla och underhålla tjänsten, för att kommunicera med dig, och för att förbättra våra tjänster. Vi delar inte din information med tredje parter, förutom de nödvändiga tjänsteleverantörer som hjälper oss att underhålla vår tjänst.
-
-<h4>Dataskydd</h4>
-Vi vidtar alla nödvändiga tekniska och organisatoriska åtgärder för att skydda dina personuppgifter mot obehörig åtkomst, ändring, spridning eller förstörelse.
-
-<h4>Dina rättigheter</h4>
-Du har rätt att begära tillgång till, korrigering eller radering av dina personuppgifter. Du har också rätt att invända mot behandlingen av dina personuppgifter och att begära begränsning av behandlingen.
-
-<h4>Kontaktinformation</h4>
-För frågor om denna policy eller dina personuppgifter, kontakta oss på Loginsite@mail.com eller vårt dataskyddsombud på Loginsite@mail.com.
-
-<h4>Ändringar i policyn</h4>
-Denna integritetspolicy kan uppdateras från tid till annan, och den senaste versionen finns alltid tillgänglig på vår webbplats.</p>
+      <p>Vi behöver spara och behandla personuppgifter om dig, såsom ditt namn, efternamn, e-postadress och lösenord. Syftet med en sådan behandling är för att kunna erbjuda dig säker och personlig åtkomst till vår webbtjänst samt för att kunna kommunicera relevanta uppdateringar och information till dig.
+</p>
+      <p>Vi har fått dina uppgifter direkt från dig när du registrerade dig på vår webbplats. Vi tillämpar vid var tid gällande integritetslagstiftning vid all behandling av personuppgifter. Den rättsliga grunden för att behandla dina personuppgifter är ditt samtycke, som du har rätt att när som helst återkalla. Dina uppgifter kommer att sparas så länge du är aktiv användare på vår webbplats och raderas inom ett år efter din senaste aktivitet.
+</p>
+      <p>De personuppgifter vi behandlar om dig delas inte med externa parter förutom nödvändiga tjänsteleverantörer som hjälper oss att underhålla och säkra vår webbplattform. Vi kommer aldrig att överföra dina uppgifter till ett land utanför EU.
+</p>
+      <p>Personuppgiftsansvarig är Loginsite AB. Du har rätt att kontakta oss om du vill ha ut information om de uppgifter vi har om dig, för att begära rättelse, överföring eller för att begära att vi begränsar behandlingen, för att göra invändningar eller begära radering av dina uppgifter. Detta gör du enklast genom att kontakta oss på Loginsite@mail.com.
+</p>
+      <p>Du når vårt dataskyddsombud på Loginsite@mail.com.</p>
+      <p>Om du har klagomål på vår behandling av dina personuppgifter har du rätt att inge klagomål till tillsynsmyndigheten Integritetsskyddsmyndigheten, IMY.</p>
     </div>
   </template>
   
